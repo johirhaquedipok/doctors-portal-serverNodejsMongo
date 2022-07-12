4f0706d2aece45da39e7db27847e1a2bd65c0e72
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-07-12 18:08:29 +0600

    primary

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+// mongodb
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongo db connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9dtzdqb.mongodb.net/?retryWrites=true&w=majority`;
+
+// create client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const servicesCollection = client
+      .db("doctors_portal")
+      .collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir());
+
+app.get("/", (req, res) => {
+  res.send("Hello From Doctor Uncle!");
+});
+
+app.listen(port, () => {
+  console.log(`Doctors App listening on port ${port}`);
+});
